93559ec0d2a9e4d65b1c52d145d871e4137ed000
mauriciomolinapicco <mauriscode@gmail.com>
2025-03-01 16:42:18 -0300

    viewsets. Api/v2

--- backend/cfehome/routers.py
+++ backend/cfehome/routers.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from products.viewsets import ProductViewSet
+from products.viewsets import ProductGenericViewSet
 
 router = DefaultRouter()
-router.register('products-abc', ProductViewSet, basename='products')
-
+router.register('products-abc', ProductGenericViewSet, basename='products')
+print(router.urls)
 urlpatterns = router.urls
--- backend/products/viewsets.py
+++ backend/products/viewsets.py
@@ -1,9 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
+    '''
+    get -> list -> Queryset
+    get -> retrieve -> Product Instance detail view
+    post -> create new instance
+    put -> update
+    patch -> partial update
+    delete -> destroy
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+
+class ProductGenericViewSet(
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet):
+    ''' This one is for only this two: 
+    get -> list -> Queryset
+    get -> retrieve -> Product Instance detail view
+    '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
