24ce02ced051280b3bfaaac0f7c2bd8229656cf7
mauriciomolinapicco <mauriscode@gmail.com>
2025-03-09 21:06:35 -0300

    url method field in serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
         fields = [
+            'url',
             'pk',
             'title',
             'content',
@@ -20,6 +23,14 @@ class ProductSerializer(serializers.ModelSerializer):
         if not isinstance(obj, Product):
             return None
         return obj.get_discount()
+
+    def get_url(self, obj):
+        # forma manual y errada de hacerlo:
+        # return f"/api/products/{obj.pk}"
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk":obj.pk}, request=request)
 
 
 class SecondaryProductSerializer(serializers.ModelSerializer):
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,8 +4,8 @@ from . import views
 product_mixin_view = views.ProductMixinView.as_view()
 
 urlpatterns = [
-    path('<int:pk>/', product_mixin_view),
+    path('<int:pk>/', product_mixin_view, name='product-detail'),
     path('<int:pk>/update/', product_mixin_view),
     path('<int:pk>/delete/', product_mixin_view),
-    path('', views.ProductListCreateAPIView.as_view())
+    path('', views.ProductListCreateAPIView.as_view(), name='product-list')
 ]
